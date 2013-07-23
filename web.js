826432ba2d299ad61388a8f84ae3d56853858645
Nathan Bryant <bryant.nathana@gmail.com>
2013-07-23 01:47:44 +0000

    testing web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.use(express.logger());
 var message = fs.readFileSync("index.html").toString("utf-8");
 
 app.get('/', function(request, response) {
-  response.send(message);
+  response.send("test");
 });
 
 var port = process.env.PORT || 5000;
